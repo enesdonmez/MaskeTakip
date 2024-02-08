9284e1df4adc9076338d667fcce6362e3fa8e4df
Enes Donmez <enesd_5034@outlook.com>
2024-02-09 00:08:11 +0300

    Entities eklendi.

---
+++ Workaround/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Vatandas vatandas = new Vatandas();
+            Console.WriteLine(vatandas.TcNo);
+        }
+
+        // Pascal Casing
+        public class Vatandas
+        {
+            public string Ad { get; set; }
+            public string Soyad { get; set; }
+            public int DogumYili { get; set; }
+            public long TcNo { get; set; } = 10;
+        }
+    }
+}
